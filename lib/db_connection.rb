5a6cee46f74dee6f7c3b3ca784a702b7abe15d66
Jeff Brubaker <jtbrubak1@gmail.com>
2017-05-08 11:46:28 -0400

    Create new db

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -3,8 +3,8 @@ require 'sqlite3'
 PRINT_QUERIES = ENV['PRINT_QUERIES'] == 'true'
 # https://tomafro.net/2010/01/tip-relative-paths-with-file-expand-path
 ROOT_FOLDER = File.join(File.dirname(__FILE__), '..')
-CATS_SQL_FILE = File.join(ROOT_FOLDER, ARGV[0])
-CATS_DB_FILE = File.join(ROOT_FOLDER, 'cats.db')
+SONGS_SQL_FILE = File.join(ROOT_FOLDER, ARGV[0])
+SONGS_DB_FILE = File.join(ROOT_FOLDER, 'songs.db')
 
 class DBConnection
   def self.open(db_file_name)
@@ -17,12 +17,12 @@ class DBConnection
 
   def self.reset
     commands = [
-      "rm '#{CATS_DB_FILE}'",
-      "cat '#{CATS_SQL_FILE}' | sqlite3 '#{CATS_DB_FILE}'"
+      "rm '#{SONGS_DB_FILE}'",
+      "cat '#{SONGS_SQL_FILE}' | sqlite3 '#{SONGS_DB_FILE}'"
     ]
 
     commands.each { |command| `#{command}` }
-    DBConnection.open(CATS_DB_FILE)
+    DBConnection.open(SONGS_DB_FILE)
   end
 
   def self.instance
---
+++ songs.sql
@@ -0,0 +1,45 @@
+CREATE TABLE songs (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  owner_id INTEGER,
+
+  FOREIGN KEY(album_id) REFERENCES album(id)
+);
+
+CREATE TABLE albums (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  artist_id INTEGER,
+
+  FOREIGN KEY(artist_id) REFERENCES artist(id)
+);
+
+CREATE TABLE artists (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO
+  artists (id, name)
+VALUES
+  (1, "The Beatles"), (2, "Miles Davis"), (3, "Prince");
+
+INSERT INTO
+  albums (id, title, artist_id)
+VALUES
+  (1, "Magical Mystery Tour", 1),
+  (2, "Abbey Road", 1),
+  (3, "In a Silent Way", 2),
+  (4, "Kind of Blue", 2);
+  (5, "Purple Rain", 3),
+  (6, "Controversy", 3);
+
+INSERT INTO
+  songs (id, title, album_id)
+VALUES
+  (1, "The Fool on the Hill", 1),
+  (2, "Come Together", 2),
+  (3, "In a Silent Way", 3),
+  (4, "So What", 4),
+  (5, "When Doves Cry", 5);
+  (6, "Controversy", 6)
